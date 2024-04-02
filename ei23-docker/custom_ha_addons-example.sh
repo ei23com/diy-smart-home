7d9d6f8606c63d934050c70a9842f3798c57fb8a
ei23com <github@fxg.li>
2024-04-02 20:23:53 +0200

    cosmedics

--- ei23-docker/custom_ha_addons-example.sh
+++ ei23-docker/custom_ha_addons-example.sh
@@ -1,7 +1,9 @@
         # $1 name / $2 url / $3 topfolder / $4 addonfolder
+        # install-ha-addons "browser_mod" "https://github.com/thomasloven/hass-browser_mod/archive/refs/heads/master.zip" "hass-browser_mod-master" "browser_mod"
         # install-ha-addons "easee" "https://codeload.github.com/fondberg/easee_hass/zip/refs/heads/master" "easee_hass-master" "easee"
         # install-ha-addons "ecoflow_cloud" "https://codeload.github.com/tolwi/hassio-ecoflow-cloud/zip/refs/heads/main" "hassio-ecoflow-cloud-main" "ecoflow_cloud"
-        # install-ha-addons "tesla" "https://codeload.github.com/alandtse/tesla/zip/refs/heads/dev" "tesla_dev" "tesla_custom"
         # install-ha-addons "localtuya" "https://codeload.github.com/rospogrigio/localtuya/zip/refs/heads/master" "localtuya-master" "localtuya"
+        # install-ha-addons "ollama_conversation" "https://github.com/ej52/hass-ollama-conversation/archive/refs/heads/main.zip" "hass-ollama-conversation-main" "ollama_conversation"
         # install-ha-addons "sonoff" "https://codeload.github.com/AlexxIT/SonoffLAN/zip/refs/heads/master" "SonoffLAN-master" "sonoff"
-        # install-ha-addons "tuya-home-assistant" "https://codeload.github.com/tuya/tuya-home-assistant/zip/refs/heads/master" "tuya-home-assistant-master" "tuya_v2"
+        # install-ha-addons "stream_assist" "https://github.com/AlexxIT/StreamAssist/archive/refs/heads/main.zip" "StreamAssist-main" "stream_assist"
+        # install-ha-addons "tesla" "https://codeload.github.com/alandtse/tesla/zip/refs/heads/dev" "tesla-dev" "tesla_custom"
